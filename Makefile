8f2d72fb857758fc46d7cb23c42cabc28d4b32c3
Tommaso Garuglieri <tommaso.garuglieri@travelappeal.com>
2019-02-15 15:06:44 +0100

    removed hash check from file copy

--- Makefile
+++ Makefile
@@ -36,8 +36,8 @@ docker-push: docker-build
 docker-build-arm:
 	docker build -t ${DOCKER_IMAGE_ARM}:${VERSION} -f ${DOCKERFILE_ARMHF} .
 
-docker-push-arm: docker-build-arm
+docker-push-arm:
 	#docker push ${DOCKER_IMAGE_ARM}:${VERSION}
-	#docker manifest create --amend ${DOCKER_IMAGE_ARM} ${DOCKER_IMAGE_ARM}:${VERSION}
+	docker manifest create --amend ${DOCKER_IMAGE_ARM} ${DOCKER_IMAGE_ARM}:${VERSION}
 	docker manifest annotate ${DOCKER_IMAGE_ARM} ${DOCKER_IMAGE_ARM}:${VERSION} --os linux --arch arm
 	docker manifest push ${DOCKER_IMAGE_ARM}
--- internal/daemon/tasks/transfer.go
+++ internal/daemon/tasks/transfer.go
@@ -18,27 +18,10 @@ func (t TransferTask) Type() string {
 func (t TransferTask) Run() error {
 	log.Infof("Copying %s to %s", t.Source, t.Destination)
 
-
-	sourceHash, err := files.HashFile(t.Source)
-
-	if err != nil {
-		return err
-	}
-
-	err = files.CopyFile(t.Source, t.Destination)
+	err := files.CopyFile(t.Source, t.Destination)
 
 	if err != nil {
 		return err
-	}
-
-	destinationHash, err := files.HashDir(t.Destination)
-
-	if err != nil {
-		return err
-	}
-
-	if sourceHash != destinationHash {
-		return fmt.Errorf("copy successfully but hashes don't match %s != %s", sourceHash, destinationHash)
 	}
 
 	fmt.Println("File copied successfully")
